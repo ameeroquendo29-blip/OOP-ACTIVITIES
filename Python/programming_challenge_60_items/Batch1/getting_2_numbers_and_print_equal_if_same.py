25a3028c0f13de6ee200d128bca2d77c8674e528
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-09 13:24:52 +0800

    typed the two conditions to follow

--- Python/programming_challenge_60_items/Batch1/getting_2_numbers_and_print_equal_if_same.py
+++ Python/programming_challenge_60_items/Batch1/getting_2_numbers_and_print_equal_if_same.py
@@ -2,4 +2,8 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 #compare 2 numbers and print equal if same
+if num1 == num2:
+    print("Equal")
 #write else statement
+else:
+    print("Not equal")
